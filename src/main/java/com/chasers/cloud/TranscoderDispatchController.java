c7b3348bc51eaabffae2a9ae5bcfa6374a6b968f
achance6 <ayden.chance@rocketmail.com>
2025-03-04 13:27:28 -0500

    Separated controller and service

--- src/main/java/com/chasers/cloud/TranscoderDispatchController.java
+++ src/main/java/com/chasers/cloud/TranscoderDispatchController.java
@@ -3,131 +3,23 @@ package com.chasers.cloud;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.services.mediaconvert.MediaConvertClient;
-import software.amazon.awssdk.services.mediaconvert.model.*;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class TranscoderDispatchController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TranscoderDispatchController.class);
+    private final TranscoderDispatchService transcoderDispatchService;
 
-    private static final String s3OutputURI = "s3://cctv-transcoded-video-storage/";
-
-    private static final String mediaConvertRoleArn = "arn:aws:iam::442426851957:role/service-role/MediaConvert_Default_Role";
+    public TranscoderDispatchController(TranscoderDispatchService transcoderDispatchService) {
+        this.transcoderDispatchService = transcoderDispatchService;
+    }
 
     @Post
     public Map<String, Object> index(@Body String s3FileLocation) {
-        MediaConvertClient mediaConvertClient = MediaConvertClient.create();
-        String id = createMediaJob(mediaConvertClient, s3FileLocation);
-        mediaConvertClient.close();
-        return Collections.singletonMap("id", id);
-    }
-
-    private static String createMediaJob(MediaConvertClient mediaConvertClient, String s3InputFileURI) {
-
-        String thumbsOutput = s3OutputURI + "thumbs/";
-        String mp4Output = s3OutputURI + "mp4/";
-
-        LOGGER.debug("MediaConvert role arn: {}", mediaConvertRoleArn);
-        LOGGER.debug("MediaConvert input file: {}", s3InputFileURI);
-        LOGGER.debug("MediaConvert output base path: {}", s3OutputURI);
-
-        OutputGroup fileMp4 = createMp4OutputGroup(mp4Output);
-        OutputGroup thumbsGroup = createThumbsOutputGroup(thumbsOutput);
-
-        JobSettings jobSettings = createJobSettings(s3InputFileURI, fileMp4, thumbsGroup);
-
-        CreateJobRequest createJobRequest = CreateJobRequest.builder()
-                .role(mediaConvertRoleArn)
-                .settings(jobSettings)
-                .build();
-
-        CreateJobResponse createJobResponse = mediaConvertClient.createJob(createJobRequest);
-
-        return createJobResponse.job().id();
-    }
-
-    private static JobSettings createJobSettings(String s3InputFileURI, OutputGroup... outputGroups) {
-        Map<String, AudioSelector> audioSelectors = new HashMap<>();
-        audioSelectors.put("Audio Selector 1", AudioSelector.builder().defaultSelection(AudioDefaultSelection.DEFAULT).offset(0).build());
-
-        return JobSettings.builder()
-                .followSource(1)
-                .inputs(
-                        Input.builder()
-                                .audioSelectors(audioSelectors)
-                                .videoSelector(VideoSelector.builder().build())
-                                .timecodeSource(InputTimecodeSource.ZEROBASED)
-                                .fileInput(s3InputFileURI)
-                                .build())
-                .outputGroups(outputGroups)
-                .build();
-    }
-
-    private static OutputGroup createMp4OutputGroup(String mp4Destination) {
-        return OutputGroup.builder().name("File Group")
-                .outputGroupSettings(
-                        OutputGroupSettings.builder()
-                                .type(OutputGroupType.FILE_GROUP_SETTINGS)
-                                .fileGroupSettings(
-                                        FileGroupSettings.builder()
-                                                .destination(mp4Destination)
-                                                .destinationSettings(
-                                                        DestinationSettings.builder()
-                                                                .s3Settings(
-                                                                        S3DestinationSettings.builder()
-                                                                                .storageClass(S3StorageClass.STANDARD)
-                                                                                .build())
-                                                                .build())
-                                                .build())
-                                .build())
-                .outputs(
-                        Output.builder().preset("ultra-low-res").nameModifier("-ultra-low-res").build(),
-                        Output.builder().preset("low-res").nameModifier("-low-res").build(),
-                        Output.builder().preset("medium-res").nameModifier("-medium-res").build(),
-                        Output.builder().preset("high-res").nameModifier("-high-res").build()
-                ).build();
-    }
-
-    private static OutputGroup createThumbsOutputGroup(String thumbsOutput) {
-        return OutputGroup.builder()
-                .name("File Group")
-                .customName("thumbs")
-                .outputGroupSettings(OutputGroupSettings.builder()
-                        .type(OutputGroupType.FILE_GROUP_SETTINGS)
-                        .fileGroupSettings(FileGroupSettings.builder()
-                                .destination(thumbsOutput).build())
-                        .build())
-                .outputs(Output.builder().extension("jpg")
-                        .containerSettings(ContainerSettings.builder()
-                                .container(ContainerType.RAW).build())
-                        .videoDescription(VideoDescription.builder()
-                                .height(480)
-                                .scalingBehavior(ScalingBehavior.DEFAULT)
-                                .sharpness(50).antiAlias(AntiAlias.ENABLED)
-                                .timecodeInsertion(
-                                        VideoTimecodeInsertion.DISABLED)
-                                .colorMetadata(ColorMetadata.INSERT)
-                                .dropFrameTimecode(DropFrameTimecode.ENABLED)
-                                .codecSettings(VideoCodecSettings.builder()
-                                        .codec(VideoCodec.FRAME_CAPTURE)
-                                        .frameCaptureSettings(
-                                                FrameCaptureSettings
-                                                        .builder()
-                                                        .framerateNumerator(1)
-                                                        .framerateDenominator(1)
-                                                        .maxCaptures(1)
-                                                        .quality(80)
-                                                        .build())
-                                        .build())
-                                .build())
-                        .build())
-                .build();
+        String jobId = transcoderDispatchService.createMediaJob(s3FileLocation);
+        return Collections.singletonMap("id", jobId);
     }
 }
+
