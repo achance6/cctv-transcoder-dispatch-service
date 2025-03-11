5c755708f632c591954a79236aa7b968641f5456
achance6 <ayden.chance@rocketmail.com>
2025-03-11 12:39:27 -0400

    Skeletonized handler

--- src/main/java/com/chasers/cloud/FunctionRequestHandler.java
+++ src/main/java/com/chasers/cloud/FunctionRequestHandler.java
@@ -1,22 +1,35 @@
 package com.chasers.cloud;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import io.micronaut.function.aws.MicronautRequestHandler;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import io.micronaut.json.JsonMapper;
+
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpStatus;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.micronaut.serde.ObjectMapper;
 import jakarta.inject.Inject;
 import java.util.Collections;
 public class FunctionRequestHandler extends MicronautRequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Inject
-    JsonMapper objectMapper;
+    ObjectMapper objectMapper;
 
     @Override
     public APIGatewayProxyResponseEvent execute(APIGatewayProxyRequestEvent input) {
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        if (HttpMethod.parse(input.getHttpMethod()) != HttpMethod.POST) {
+            response.setStatusCode(HttpStatus.NOT_FOUND.getCode());
+            return response;
+        }
         try {
-            String json = new String(objectMapper.writeValueAsBytes(Collections.singletonMap("message", "Hello World")));
-            response.setStatusCode(200);
+            S3EventNotification s3EventNotification = objectMapper.readValue(input.getBody(), S3EventNotification.class);
+        } catch (IOException e) {
+            response.setStatusCode(HttpStatus.BAD_REQUEST.getCode());
+            return response;
+        }
+        try {
+            String json = new String(objectMapper.writeValueAsBytes(Collections.singletonMap("jobId", "")));
+            response.setStatusCode(201);
             response.setBody(json);
         } catch (IOException e) {
             response.setStatusCode(500);
